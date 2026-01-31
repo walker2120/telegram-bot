ad2683c79a9270f273e87d2d2146e6d7327e8fcc
walker2120 <60056726+walker2120@users.noreply.github.com>
2026-01-31 04:17:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import os import time import requests TOKEN = os.getenv("BOT_TOKEN") CHAT_ID = os.getenv("CHAT_ID") URL = f"https://api.telegram.org/bot{TOKEN}" def send_message(text): requests.post( f"{URL}/sendMessage", data={"chat_id": CHAT_ID, "text": text} ) if __name__ == "__main__": send_message("🤖 Bot activo en la nube. Todo listo 🚀") while True: time.sleep(60)
+import os
+import time
+import requests
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+URL = f"https://api.telegram.org/bot{TOKEN}"
+
+def send_message(text):
+    requests.post(
+        f"{URL}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": text}
+    )
+
+if name == "__main__":
+    send_message("🤖 Bot activo en la nube. Todo listo 🚀")
+    while True:
+        time.sleep(60)
